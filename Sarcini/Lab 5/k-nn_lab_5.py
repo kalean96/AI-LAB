c3045b3c02d448c808a34cee052d6338f96509d2
bumbutudor <fcimia122@gmail.com>
2019-11-19 21:48:19 +0200

    lab 5 added

---
+++ Sarcini/Lab 5/k-nn_lab_5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Algoritmul K Nearest Neighbors
+
+# ## Sarcina
+# 
+
+# Scrieți un program Python folosind Scikit-learn. Împărțiți setul de date 'iris.csv' (path dataset/iris.csv) în 70% date de antrenare și 30% date de testare. Din totalul de 150 de înregistrări, setul de antrenare va conține 105 înregistrări, iar setul de testare va conține 45 dintre aceste înregistrări. Presupuneți răspunsul corect pentru setul de date de testare care are urmatoarele artibute ('sepal_length', 'sepal_width', 'petal_length', 'petal_width') folosind algoritmul k-Nearest Neighbors. Utilizați k = 5 (număr de vecini).
+# 
+# Referinte: https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn
+# 
+# Solutie: http://81.180.84.246:8888/notebooks/AI-LAB/Lab%205/lab_5_solutie.ipynb
+# 
+# Parola: HDOHP cifrata cu paternul (alfabet: 'abcdefghijklmnopqrstuvwxy'  -> alfabet cifrat: '??qgiumeaylnof?xjkrcvstzwb')
+
+# In[ ]:
+
+
+
+
